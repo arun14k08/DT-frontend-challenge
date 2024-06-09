de6abbf5e5ba96a24fd886e588683739ff75efbd
ARUN KUMAR D <arun14k08@gmail.com>
2024-06-09 18:32:49 +0530

    feat: border color fixed

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const InputBox = ({title}) => {
     return (
-        <div className="rounded-lg min-h-[95px] bg-[rgba(0,0,0,.05)] border-[#333333] border-[1px]">
+        <div className="rounded-lg min-h-[95px] bg-[rgba(0,0,0,.05)] border-[rgba(51,51,51,0.15)] border-[1px]">
             <p className="my-[4px] mx-[7px]">{title}</p>
-            <div className="rounded-lg shadow-lg  bg-white border-[#333333] border-[1px]">
+            <div className="rounded-lg shadow-lg  bg-white border-[#33333327] border-[1px]">
                 <textarea name="" id="" className="w-full rounded-lg px-[7px] py-[7px]" placeholder="Enter Text here"/>
             </div>
         </div>
